d484c20088a03c7d1210a755678eea388f2a827c
Peace Akinola <akinolapo@gmail.com>
2022-05-11 17:45:51 +0100

    About Components

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,10 +1,49 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.JPG'
+import{FaAward} from 'react-icons/fa'
+import{FiUsers} from 'react-icons/fi'
+import{VscFolderLibrary} from 'react-icons/vsc'
 
 const about = () => {
   return (
     <section id='about'>
-      
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-img">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>4+ Years Working</small>
+            </article>
+
+            <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>20 + Worldwide</small>
+            </article>
+
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>50+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+          Scrupulous web developer with over 4 years of front-end experience and admiration for responsive website design and a firm believer in the mobile-first approach. 
+          </p>
+          <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
     </section>
   )
 }
