9dcb72f9b0f8459b2f07f653a7592655ff804dc3
Peace Akinola <akinolapo@gmail.com>
2022-05-11 11:45:02 +0100

    header component edited

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocials from './headersocials'
 
 const header = () => {
   return (
-    <div>header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Peace Akinola</h1>
+        <h5 className="text-light">Frontend Developer</h5>
+        <CTA/>
+        <HeaderSocials/>
+
+        <div className="me">
+          <img src={ME} alt='me'></img>
+        </div>
+
+        <a href='#contact' className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
---
+++ src/components/header/headersocials.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithubAlt} from 'react-icons/fa'
+import {FiDribbble} from 'react-icons/fi'
+
+const headersocials = () => {
+  return (
+    <div className='header__socials'>
+      <a href='https://www.linkedin.com/in/akinolapo' target= "new"><BsLinkedin/></a>
+      <a href='https://github.com/akinolapo' target= "new"><FaGithubAlt/></a>
+      <a href='https://twitter.com/akinolapo_' target= "new"><FiDribbble/></a>
+    </div>
+  )
+}
+
+export default headersocials
