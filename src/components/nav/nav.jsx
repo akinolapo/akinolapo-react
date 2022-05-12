875c791000f6f3000ee1fbb37bcea04df3985515
akinolapo <akinolapo@gamil.com>
2022-05-12 20:05:04 +0100

    rearrage menu

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -12,8 +12,8 @@ const Nav = () => {
       <a href='#' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
       <a href='#about' onClick={() => setActiveNav('#about')}  className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
       <a href='#experience' onClick={() => setActiveNav('#experience')}  className={activeNav === '#experience' ? 'active' : ''}><AiOutlineBook/></a>
-      <a href='#portfolio' onClick={() => setActiveNav('#portfolio')}  className={activeNav === '#portfolio' ? 'active' : ''}><BiBriefcase/></a>
       <a href='#services' onClick={() => setActiveNav('#services')}  className={activeNav === '#services' ? 'active' : ''}><RiServiceLine/></a>
+      <a href='#portfolio' onClick={() => setActiveNav('#portfolio')}  className={activeNav === '#portfolio' ? 'active' : ''}><BiBriefcase/></a>
       <a href='#contact' onClick={() => setActiveNav('#contact')}  className={activeNav === '#contact' ? 'active' : ''}><AiOutlineMessage/></a>
     </nav>
   )
