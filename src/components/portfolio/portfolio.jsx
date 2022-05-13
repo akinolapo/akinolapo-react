4160c9b228bb665356c44bbe26fa58cbb71237a2
akinolapo <akinolapo@gamil.com>
2022-05-13 12:49:21 +0100

    portfolio component completed

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,11 +1,77 @@
 import React from 'react'
 import './portfolio.css'
 
+// DATA ARRAY FOR PORTFOLIO DATA
+
+const data =[
+  {
+  id: 1,
+  image: process.env.PUBLIC_URL+`assets/portfolio1.png`,
+  title: 'Peighton pryor raisey website',
+  github: '#',
+  demo: 'https://p8on.net'
+},
+{
+  id: 2,
+  image: process.env.PUBLIC_URL+`assets/portfolio2.png`,
+  title: 'Darius collection eCommerce Website',
+  github: '#',
+  demo: 'https://www.dariuscordell.com'
+},
+{
+  id: 3,
+  image: process.env.PUBLIC_URL+`assets/portfolio3.png`,
+  title: 'Sheffield Eye Care WordPress Website',
+  github: '#',
+  demo: 'https://sheffieldeyecare.co.uk'
+},
+{
+  id: 4,
+  image: process.env.PUBLIC_URL+`assets/portfolio4.png`,
+  title: 'Blog writing on WordPress Help',
+  github: '#',
+  demo: 'https://www.bookwebmaster.com'
+},
+{
+  id: 5,
+  image: process.env.PUBLIC_URL+`assets/portfolio5.png`,
+  title: 'Weather Web App with JavaScript',
+  github: 'https://github.com/akinolapo/cityWeatherApp',
+  demo: 'https://weatherappforcities.netlify.app'
+},
+{
+  id: 6,
+  image: process.env.PUBLIC_URL+`assets/portfolio6.png`,
+  title: 'Apple Music Player UI With HTML & CSS',
+  github: 'https://github.com/akinolapo/ios-music-player',
+  demo: 'https://ios-music-player.netlify.app/'
+}
+]
+
 const portfolio = () => {
   return (
     <section id='portfolio'>
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+       {
+         data.map(({id, image, title, github, demo})=>{
+           return (
+            <article key={id} className='portfolio__item'>
+            <div className="portfolio__item-image">
+              <img src={image} alt={title}></img>
+            </div>
+            <h3>{title}</h3>
+            <div className="portfolio__item-cta">
+            <a href={github} rel="noreferrer" target='_blank' className='btn'>Github</a>
+            <a href={demo} rel="noreferrer" target='_blank' className='btn btn btn-primary'>Live Demo</a>
+            </div>
+          </article> 
+           )
+         })
+       }     
+      </div>
     </section>
   )
 }
