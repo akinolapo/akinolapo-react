aa7f686c9e335cb7c9f9c51faaa0dd632ad30dd4
Peace Akinola <akinolapo@gmail.com>
2022-05-28 16:31:16 +0100

    meta description

---
+++ .history/src/components/header/header_20220528162446.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './header.css'
+import CTA from './CTA'
+// import ME from '../../assets/me.png'
+import HeaderSocials from './headersocials'
+
+const header = () => {
+  return (
+    <header>
+      <div className="container header__container">
+        <h4>Hello I'm</h5>
+        <h1>Peace Akinola</h1>
+        <h5 className="text-light">Frontend Developer</h5>
+        <CTA/>
+        <HeaderSocials/>
+
+        <div className="me">
+          <img src={process.env.PUBLIC_URL+`assets/me.png`} alt='me'></img>
+        </div>
+
+        <a href='#contact' className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
+  )
+}
+
+export default header
